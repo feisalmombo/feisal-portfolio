aaa10bea0aa20e6f242df48627779c484729cc5f
Feisal Mombo <feisalmombo29@gmail.com>
2021-02-04 10:52:57 +0300

    modify users_roles table

--- database/migrations/2021_02_04_074612_create_users_roles_table.php
+++ database/migrations/2021_02_04_074612_create_users_roles_table.php
@@ -16,6 +16,14 @@ class CreateUsersRolesTable extends Migration
         Schema::create('users_roles', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
+            $table->softDeletes();
+            $table->integer('user_id')->unsigned();
+            $table->integer('role_id')->unsigned();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+
+            $table->primary(['user_id','role_id']);
         });
     }
 
