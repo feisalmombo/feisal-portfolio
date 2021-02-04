3ff4d096b437c2155368b67978b949711f9b1eb2
Feisal Mombo <feisalmombo29@gmail.com>
2021-02-04 10:58:09 +0300

    modify roles_permissions table

--- database/migrations/2021_02_04_075500_create_roles_permissions_table.php
+++ database/migrations/2021_02_04_075500_create_roles_permissions_table.php
@@ -16,6 +16,14 @@ class CreateRolesPermissionsTable extends Migration
         Schema::create('roles_permissions', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
+            $table->softDeletes();
+            $table->integer('role_id')->unsigned();
+            $table->integer('permission_id')->unsigned();
+
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
+
+            $table->primary(['role_id','permission_id']);
         });
     }
 
