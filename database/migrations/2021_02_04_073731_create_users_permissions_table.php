be691b485c2c39ae0a27ed5bb6cd62e2b14fe3a5
Feisal Mombo <feisalmombo29@gmail.com>
2021-02-04 10:51:05 +0300

    modify users_permissions

--- database/migrations/2021_02_04_073731_create_users_permissions_table.php
+++ database/migrations/2021_02_04_073731_create_users_permissions_table.php
@@ -16,6 +16,14 @@ class CreateUsersPermissionsTable extends Migration
         Schema::create('users_permissions', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
+            $table->softDeletes();
+            $table->integer('user_id')->unsigned();
+            $table->integer('permission_id')->unsigned();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
+
+            $table->primary(['user_id','permission_id']);
         });
     }
 
