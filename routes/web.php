88de00147f4f92426ffa223e16b6bb00c6358c51
Feisal Mombo <feisalmombo29@gmail.com>
2021-02-04 18:03:08 +0300

    fix web.php

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,86 @@
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Auth::routes();
+// Route::get('/home', 'HomeController@index')->name('home');
+
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+// ROUTE FOR AUTHENTICATION
+Route::get('login', [
+	'as' => 'login',
+	'uses' => 'Auth\LoginController@showLoginForm'
+  ]);
+  Route::post('login', [
+	'as' => '',
+	'uses' => 'Auth\LoginController@login'
+  ]);
+  Route::post('logout', [
+	'as' => 'logout',
+	'uses' => 'Auth\LoginController@logout'
+  ]);
+  Route::post('password/email', [
+	'as' => 'password.email',
+	'uses' => 'Auth\ForgotPasswordController@sendResetLinkEmail'
+  ]);
+  Route::get('password/reset', [
+	'as' => 'password.request',
+	'uses' => 'Auth\ForgotPasswordController@showLinkRequestForm'
+  ]);
+  Route::post('password/reset', [
+	'as' => 'password.update',
+	'uses' => 'Auth\ResetPasswordController@reset'
+  ]);
+  Route::get('password/reset/{token}', [
+	'as' => 'password.reset',
+	'uses' => 'Auth\ResetPasswordController@showResetForm'
+  ]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+// ROUTE FOR NEW VIEW FOR CHANGE PASSWORD
+// Route::get('/change_password', function () {
+//     return view('auth.passwords.new_user_change_pwd');
+// });
+
+// ROUTE FOR CHANGE PASSWORD
+// Route::post('/change_password', 'ChangePasswordController@updateNewuser');
+
+
+// Route::resource('/change-password', 'ChangePasswordController');
+// Route::post('/change-password', 'ChangePasswordController@update');
+
+// ROUTE FOR CHECKUSERSTATUS MIDDLEWARE
+Route::group(['middleware' => 'CheckUserStatus'], function () {
+
+    // ROUTE FOR VALIDATEBUTTONHISTORY MIDDLEWARE
+    Route::group(['middleware' => 'ValidateButtonHistory'], function () {
+
+        // ROUTE FOR MIDDLEWARE AUTH
+        Route::group(['middleware' => 'auth'], function () {
+
+            // ROUTE FOR HOME
+            Route::get('/home', 'HomeController@index')->name('home');
+
+            // ROUTES FOR PERMISSIONS
+            Route::get('/settings/manage_users/permissions/entrust_user', 'PermissionsController@entrust_user');
+            Route::get('/settings/manage_users/permissions/entrust', 'PermissionsController@entrust');
+            Route::post('/settings/manage_users/permissions/entrust_usr', 'PermissionsController@entrust_user_permissions');
+            Route::get('/settings/manage_users/permissions/entrustRole', 'PermissionsController@entrust_roles');
+            Route::post('/settings/manage_users/permissions/entrust_role_permissions', 'PermissionsController@entrust_role_permissions');
+            Route::get('/settings/manage_users/permissions/entrust_role', 'PermissionsController@entrust_role');
+            Route::resource('/settings/manage_users/permissions/', 'PermissionsController');
+
+            // ROUTES FOR ROLES
+            Route::get('/settings/manage_users/roles/entrust', 'RolesController@get_roles');
+            Route::post('/settings/manage_users/roles/entrust', 'RolesController@post_roles');
+            Route::get('/settings/manage_users/roles/add', 'RolesController@add');
+            Route::resource('/settings/manage_users/roles', 'RolesController');
+        });
+    });
+});
+
