841a2506a723ec066de3a03f2ed5ffb33beed59a
Feisal Mombo <feisalmombo29@gmail.com>
2021-02-09 14:05:21 +0300

    add model and migration for testimonial

---
+++ database/migrations/2021_02_09_110157_create_testimonials_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTestimonialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('testimonials', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('personal_name');
+            $table->string('testimonial_description');
+            $table->string('testimonial_image');
+            $table->string('testimonial_occupation');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('testimonials');
+    }
+}
