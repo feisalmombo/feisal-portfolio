260f72caedeb22b3babc2a8c85c80163fc5f9902
Feisal Mombo <feisalmombo29@gmail.com>
2021-02-09 14:00:30 +0300

    add model and migration for latest news

---
+++ database/migrations/2021_02_09_105648_create_latest_news_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLatestNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('latest_news', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('news_title');
+            $table->string('news_image');
+            $table->string('news_link');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('latest_news');
+    }
+}
