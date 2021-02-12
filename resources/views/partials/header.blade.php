cc464bd5bbb60bf126a161c944aa796921dee8e8
Feisal Mombo <feisalmombo29@gmail.com>
2021-02-12 12:39:58 +0300

    add logo.blade.php within partials folder

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -60,15 +60,7 @@
 
     <header class="main-header">
       <!-- Logo -->
-      {{-- @if(Auth::user()->id) --}}
-      <a href="{{ url('/') }}" class="logo">
-        <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><strong style="color:#ffffff">My Portfolio</strong></span>
-        <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><strong style="color:#ffffff">My Portfolio</strong></span>
-      </a>
-      {{-- @endif --}}
-
+      @include('partials.logo')
 
       <!-- Header Navbar: style can be found in header.less -->
       <nav class="navbar navbar-static-top">
---
+++ resources/views/partials/logo.blade.php
@@ -0,0 +1,6 @@
+<a href="{{ url('/') }}" class="logo">
+    <!-- mini logo for sidebar mini 50x50 pixels -->
+    <span class="logo-mini"><strong style="color:#ffffff">My Portfolio</strong></span>
+    <!-- logo for regular state and mobile devices -->
+    <span class="logo-lg"><strong style="color:#ffffff">My Portfolio</strong></span>
+</a>
