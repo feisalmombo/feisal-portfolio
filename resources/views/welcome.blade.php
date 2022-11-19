067af31b9b98aa5845f91cc04572b292e582736c
feisalmombo29 <feisalmombo29@gmail.com>
2022-11-19 18:30:37 +0300

    WIP

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1015,7 +1015,7 @@
                                     </p>
                                 </div>
                                 <div class="testi-pos">
-                                    <img src="{{ asset('portfolio/images/img_avatar.png') }}" alt="" class="img-fluid rounded-circle mb-3" />
+                                    <img src="{{ asset('portfolio/images/avatars/avatar6.jpg') }}" alt="" class="img-fluid rounded-circle mb-3" />
                                     <h4>Khadija Mohammed</h4>
                                     <span>Pilot</span>
                                 </div>
