e666e0c62e62daaeecb68de6305d80cc153b4e86
feisalmombo29 <feisalmombo29@gmail.com>
2022-11-22 13:21:48 +0300

    WIP

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,12 +2,12 @@
 <html lang="zxx">
 
 <head>
-	<title>Feisal Mombo | Login</title>
+	<title>My Portfolio | Login</title>
 	<!-- Meta tag Keywords -->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="UTF-8" />
-	<meta name="keywords"
-		content="Feisal Mombo Login"/>
+	<meta name="keywords" content="My Portfolio"/>
+
 	<script>
 		addEventListener("load", function () {
 			setTimeout(hideURLbar, 0);
@@ -17,6 +17,7 @@
 			window.scrollTo(0, 1);
 		}
 	</script>
+
 	<!-- // Meta tag Keywords -->
 	<!--/Style-CSS -->
 	<link rel="stylesheet" href="{{ asset('portfolio/dash/css/style.css') }}" type="text/css" media="all" />
@@ -32,7 +33,7 @@
 				<div class="d-grid forms23-grids">
 					<div class="form23">
 						<div class="main-bg">
-							<h6 class="sec-one">Feisal Mombo</h6>
+							<h6 class="sec-one">My Portfolio</h6>
 							<div class="speci-login first-look">
 								<img src="{{ asset('portfolio/dash/images/user.png') }}" alt="user" class="img-responsive">
 							</div>
@@ -52,7 +53,7 @@
 				</div>
 				<div class="w3l-copy-right text-center">
 					<p>&copy; {{date('Y')}} My Portfolio. All rights reserved | Developed by
-						<a href="#" target="_blank"><strong style="color: #ffffff">Feisal Mombo</strong></a></p>
+						<a href="#" target="_blank"><strong style="color: #ffffff">Feisal Suleiman Mombo</strong></a></p>
 				</div>
 			</div>
 		</div>
