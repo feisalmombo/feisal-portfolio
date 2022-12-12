8e1aaeb7953e6006916da432be12095e7e6819f6
feisalmombo <feisalmombo29@gmail.com>
2022-12-12 12:27:09 +0300

    WIP

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -15,17 +15,23 @@
 <!--JQueryFile -->
 <!-- Bootstrap 3.3.7 -->
 <script src="{{asset('portfolio/temp/bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+
 <!-- DataTables -->
 <script src="{{asset('portfolio/temp/bower_components/datatables.net/js/jquery.dataTables.min.js')}}"></script>
 <script src="{{asset('portfolio/temp/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js')}}"></script>
+
 <!-- SlimScroll -->
 <script src="{{asset('portfolio/temp/bower_components/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script>
+
 <!-- FastClick -->
 <script src="{{asset('portfolio/temp/bower_components/fastclick/lib/fastclick.js')}}"></script>
+
 <!-- AdminLTE App -->
 <script src="{{asset('portfolio/temp/dist/js/adminlte.min.js')}}"></script>
+
 <!-- AdminLTE for demo purposes -->
 <script src="{{asset('portfolio/temp/dist/js/demo.js')}}"></script>
+
 <!-- AdminLTE for requests funs -->
 <!-- page script -->
 <script>
